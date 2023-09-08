aeb8c4cd9c90e6b80f01c221e5f02afccdd09738
FanFusheng <ffs@ppno.net>
2023-09-09 04:17:07 +0800

    feat(nbio): 编写 echo 示例
    Signed-off-by: FanFusheng <ffs@ppno.net>

--- internal/app/nbio/echo/client.go
+++ internal/app/nbio/echo/client.go
@@ -1,9 +1,23 @@
 package echo
 
+import (
+	"github.com/fsyyft-go/example/pkg/sys"
+)
+
+var (
+	_ sys.Runnable = (*client)(nil)
+)
+
 type (
 	client struct { //nolint:unused
 	}
 )
 
 func (c *client) Run() { //nolint:unused
+}
+
+func NewClient() *client {
+	c := &client{}
+
+	return c
 }
--- internal/app/nbio/echo/server.go
+++ internal/app/nbio/echo/server.go
@@ -1,9 +1,23 @@
 package echo
 
+import (
+	"github.com/fsyyft-go/example/pkg/sys"
+)
+
+var (
+	_ sys.Runnable = (*server)(nil)
+)
+
 type (
 	server struct { //nolint:unused
 	}
 )
 
 func (s *server) Run() { //nolint:unused
+}
+
+func NewServer() *server {
+	s := &server{}
+
+	return s
 }
