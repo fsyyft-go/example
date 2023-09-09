92c0abd1d96f680357e453373ed6fc0c5a135cb9
FanFusheng <ffs@ppno.net>
2023-09-09 18:15:41 +0800

    feat: 增加 RunnableWithContext 接口
    Signed-off-by: FanFusheng <ffs@ppno.net>

--- pkg/sys/runnable.go
+++ pkg/sys/runnable.go
@@ -1,9 +1,19 @@
 package sys
 
+import (
+	"context"
+)
+
 type (
 	// Runnable 可运行的应用程序，包含有 Run 方法。
 	Runnable interface {
 		// Run 运行程序。
 		Run()
+	}
+
+	// RunnableWithContext 可运行的应用程序，包含有 RunContext 方法。
+	RunnableWithContext interface {
+		// RunContext 运行程序，带有上下文信息。
+		RunContext(ctx context.Context)
 	}
 )
